8c6c61cceba447a9f6ff16164e6928b3a3bd303a
khalig khalig jafaroc <xaliq.ceferov199653@gmail.com>
2024-01-09 20:27:54 +0000

    end

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units=metric`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units=metric`;
             async function myweather() {
-                let responce = await fetch(url)
-                let data = await responce.json()
+                let responce = await fetch(url);
+                let data = await responce.json();
                 $("#city").html(data.name);
                 $("#temp").html(data.main.temp);
                 $("#feels").html(data.main.feels_like);
                 $("#myWeather").val("");
-                console.log(data)
+                console.log(data);
             }
             myweather()
         }
@@ -21,15 +21,15 @@ $(document).ready(function () {
 
     $("#btn").click(function (e) {
         e.preventDefault();
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units=metric`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units=metric`;
         async function myweather() {
-            let responce = await fetch(url)
-            let data = await responce.json()
+            let responce = await fetch(url);
+            let data = await responce.json();
             $("#city").html(data.name);
             $("#temp").html(data.main.temp);
             $("#feels").html(data.main.feels_like);
             $("#myWeather").val("");
-            console.log(data)
+            console.log(data);
         }
         myweather()
     });
